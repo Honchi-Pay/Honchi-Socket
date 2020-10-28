9db4c87e062b470746821f9733b6408981ce5249
Hanif <marbling1293@naver.com>
2020-10-28 11:15:16 +0900

    [ADD] Security

---
+++ src/main/java/com/honchi/socket/security/JwtTokenProvider.java
@@ -0,0 +1,27 @@
+package com.honchi.socket.security;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${auth.jwt.secret}")
+    private String secretKey;
+
+    public boolean validationToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secretKey)
+                    .parseClaimsJws(token).getBody().getSubject();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getEmail(String token) {
+        return Jwts.parser().setSigningKey(secretKey)
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+}
