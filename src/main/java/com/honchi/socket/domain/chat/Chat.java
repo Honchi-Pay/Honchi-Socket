d8e2021136a27985a530115dccd97ec831826bd9
Hanif <marbling1293@naver.com>
2020-10-31 22:05:54 +0900

    [ADD] Chat Entity

---
+++ src/main/java/com/honchi/socket/domain/chat/Chat.java
@@ -0,0 +1,30 @@
+package com.honchi.socket.domain.chat;
+
+import com.honchi.socket.domain.chat.enums.Authority;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Chat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String roomId;
+
+    @Column(nullable = false)
+    private Integer userId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
+}
