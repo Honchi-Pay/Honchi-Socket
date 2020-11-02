cd556af31e68571b00b3405cf8a464cd0253c0a2
Hanif <marbling1293@naver.com>
2020-11-02 19:24:29 +0900

    [ADD] Lombok Annotation

--- src/main/java/com/honchi/socket/payload/MessageRequest.java
+++ src/main/java/com/honchi/socket/payload/MessageRequest.java
@@ -1,8 +1,12 @@
 package com.honchi.socket.payload;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MessageRequest {
 
     private String roomId;
