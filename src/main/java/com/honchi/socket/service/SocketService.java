c7f6802edd8a67f458bc363a0082e2fd5cd8d70b
Hanif <marbling1293@naver.com>
2020-11-12 20:15:52 +0900

    [ADD] changeTitle, leaveRoom

--- src/main/java/com/honchi/socket/service/SocketService.java
+++ src/main/java/com/honchi/socket/service/SocketService.java
@@ -1,6 +1,7 @@
 package com.honchi.socket.service;
 
 import com.corundumstudio.socketio.SocketIOClient;
+import com.honchi.socket.payload.ChangeTitleRequest;
 import com.honchi.socket.payload.MessageRequest;
 
 public interface SocketService {
@@ -8,5 +9,7 @@ public interface SocketService {
     void connect(SocketIOClient client);
     void disConnect(SocketIOClient client);
     void joinRoom(SocketIOClient client, String room);
+    void leaveRoom(SocketIOClient client, String room);
+    void changeTitle(SocketIOClient client, ChangeTitleRequest changeTitleRequest);
     void send(SocketIOClient client, MessageRequest messageRequest);
 }
