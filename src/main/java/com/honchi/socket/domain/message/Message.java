55bbc7fa00d948484b0f7d802c2bf2f5aa37cb5c
Hanif <marbling1293@naver.com>
2020-11-30 08:53:27 +0900

    [ADD] @Column annotation

--- src/main/java/com/honchi/socket/domain/message/Message.java
+++ src/main/java/com/honchi/socket/domain/message/Message.java
@@ -3,10 +3,7 @@ package com.honchi.socket.domain.message;
 import com.honchi.socket.domain.message.enums.MessageType;
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
@@ -20,12 +17,16 @@ public class Message {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(nullable = false)
     private Integer userId;
 
+    @Column(nullable = false)
     private String chatId;
 
+    @Column(nullable = false)
     private String message;
 
+    @Enumerated(EnumType.STRING)
     private MessageType messageType;
 
     private Integer readCount;
