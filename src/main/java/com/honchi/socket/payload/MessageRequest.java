fa9bd496e8a6a6726ab128101b988de2910bdb0c
Hanif <marbling1293@naver.com>
2020-11-30 16:15:57 +0900

    [FIX] roomId to chatId

--- src/main/java/com/honchi/socket/payload/MessageRequest.java
+++ src/main/java/com/honchi/socket/payload/MessageRequest.java
@@ -1,14 +1,11 @@
 package com.honchi.socket.payload;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class MessageRequest {
 
-    private String roomId;
-    private String Message;
+    private String chatId;
+
+    private String message;
 }
