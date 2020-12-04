e2addbaf8c1f7c3beea401ae79137f4e2a1eeea7
Hanif <marbling1293@naver.com>
2020-12-05 00:03:53 +0900

    [ADD] @JsonIgnoreProperties

--- src/main/java/com/honchi/socket/payload/MessageRequest.java
+++ src/main/java/com/honchi/socket/payload/MessageRequest.java
@@ -1,13 +1,14 @@
 package com.honchi.socket.payload;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MessageRequest {
 
     private String chatId;
