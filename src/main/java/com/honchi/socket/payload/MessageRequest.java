6f6a33f4c490cb975139031e43139b7ab4f1d664
Hanif <marbling1293@naver.com>
2020-12-04 19:15:31 +0900

    [FIX] messageRequest

--- src/main/java/com/honchi/socket/payload/MessageRequest.java
+++ src/main/java/com/honchi/socket/payload/MessageRequest.java
@@ -1,17 +1,22 @@
 package com.honchi.socket.payload;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class MessageRequest {
 
     private String chatId;
 
     private String message;
+
+    public MessageRequest() {
+
+    }
+
+    public MessageRequest(String chatId, String message) {
+        this.chatId = chatId;
+        this.message = message;
+    }
 }
