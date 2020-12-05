422af6aa4ec055f25ad06e244ceca1bb4476fc7d
Hanif <marbling1293@naver.com>
2020-12-05 19:20:55 +0900

    [DELETE] JsonIgnoreProperties

--- src/main/java/com/honchi/socket/payload/MessageRequest.java
+++ src/main/java/com/honchi/socket/payload/MessageRequest.java
@@ -1,6 +1,5 @@
 package com.honchi.socket.payload;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +7,6 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class MessageRequest {
 
     private String chatId;
