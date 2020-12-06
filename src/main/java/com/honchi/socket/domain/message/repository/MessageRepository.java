8b14e7284c4e6cc7b9d17b1176d5c17ff2a574bf
Hanif <marbling1293@naver.com>
2020-12-06 15:34:57 +0900

    [DELETE] 불필요한 코드 제거

--- src/main/java/com/honchi/socket/domain/message/repository/MessageRepository.java
+++ src/main/java/com/honchi/socket/domain/message/repository/MessageRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MessageRepository extends CrudRepository<Message, Integer> {
-    Message findTop1ByChatIdOrderByTimeDesc(String chatId);
 }
